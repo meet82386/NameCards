7f38b2aeff37790fbf0917e305f32026d30d8a4b
Meet Thummar <thummarmeet15@gmail.com>
2023-03-24 18:02:02 +0530

    Saperated Avatar from Card

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,6 +14,18 @@ function App() {
         number = {contacts[0].phone}
         email = {contacts[0].email}
       />
+      <Card 
+        name = {contacts[1].name}
+        imgLink = {contacts[1].imgURL}
+        number = {contacts[1].phone}
+        email = {contacts[1].email}
+      />
+      <Card 
+        name = {contacts[2].name}
+        imgLink = {contacts[2].imgURL}
+        number = {contacts[2].phone}
+        email = {contacts[2].email}
+      />
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,12 @@
 import React from "react";
+import Avatar from "./Avatar";
 
 function Card(props) {
     return <div className="card">
         <div className="top">
           <h2 className="name">{props.name}</h2>
-          <img  
-            className="circle-img"
-            src= {props.imgLink}
-            alt="avatar_img"
+          <Avatar 
+            imgLink = {props.imgLink}
           />
         </div>
         <div className="bottom">
